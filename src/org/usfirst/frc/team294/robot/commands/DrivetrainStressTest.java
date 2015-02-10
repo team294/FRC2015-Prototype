96bfc2898705799c348cb3dd060b0868f4c31a36
Nate Schickler <nate@schickler.org>
2015-02-09 20:53:36 -0800

    stress test

--- src/org/usfirst/frc/team294/robot/commands/DrivetrainStressTest.java
+++ src/org/usfirst/frc/team294/robot/commands/DrivetrainStressTest.java
@@ -3,48 +3,53 @@ package org.usfirst.frc.team294.robot.commands;
 import org.usfirst.frc.team294.robot.Robot;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class DrivetrainStressTest extends CommandGroup {
+public class DrivetrainStressTest extends Command {
 
-	public  DrivetrainStressTest() {
-		// Add Commands here:
-		// e.g. addSequential(new Command1());
-		//      addSequential(new Command2());
-		// these will run in order.
+	private boolean backwards=false;
+	private double speedMultiplier=1.0;
+	Timer t=new Timer();
 
-		// To run multiple commands at the same time,
-		// use addParallel()
-		// e.g. addParallel(new Command1());
-		//      addSequential(new Command2());
-		// Command1 and Command2 will run in parallel.
-
-		// A command group will require all of the subsystems that each member
-		// would require.
-		// e.g. if Command1 requires chassis, and Command2 requires arm,
-		// a CommandGroup containing them would require both the chassis and the
-		// arm.
+	public DrivetrainStressTest() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
 		requires(Robot.drivetrain);
-		Timer inner=new Timer();
-		Timer outer=new Timer();
-		double speedIterator=1.0;
-		while(outer.get()<3600.0){
-			while(inner.get()<2.0){
-				addSequential(new AutoDriveStraight(100.0, 1*speedIterator));
-			}
-			while(inner.get()>2.0){
-				addSequential(new AutoDriveStraight(-100.0, 1*speedIterator));
-			}
-			speedIterator-=0.3;
-			inner.stop();
-			inner.reset();
-			inner.start();
-			if(speedIterator<0.2){
-				speedIterator=1.0;
-			}
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		t.reset();
+		t.start();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		System.out.println(t.get());
+		if(t.get()<20.0){
+			Robot.drivetrain.tankDrive(1.0*speedMultiplier, 1.0*speedMultiplier);
+		}else{
+			t.stop();
+			t.reset();
+			t.start();
+			speedMultiplier*=-1;
 		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
 	}
 }
